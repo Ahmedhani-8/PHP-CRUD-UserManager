6b658f03c38e6f2f706318c7766493ef24a43383
Ahmed Hani <ahmedflu8@gmail.com>
2026-01-06 15:34:58 +0200

    Fix db_conn.php for Back4app MySQL

--- src/db_conn.php
+++ src/db_conn.php
@@ -1,13 +1,14 @@
 <?php
-$servername = "localhost"; // غيّر لـ localhost في Back4app
-$username = "root";
-$password = ""; // فارغ للـ container
-$dbname = "php-crud";
+$servername = "localhost"; // Back4app نفس الحاوية
+$username   = "root";
+$password   = "";          // فارغ للـ container
+$dbname     = "php-crud";
 
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
-echo "Connected successfully";
+
+// echo "Connected successfully";
 ?>
