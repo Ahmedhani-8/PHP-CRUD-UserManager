ff012a4de2edaa8b07e42b5c595483ce51dfa2c4
Ahmed Hani <ahmedflu8@gmail.com>
2026-01-07 10:09:42 +0200

    Fix db connection

--- src/db_conn.php
+++ src/db_conn.php
@@ -1,16 +1,11 @@
 <?php
-$servername = "host.docker.internal"; // مهم: حتى تصل الحاوية لـ MySQL على جهازك
-$username   = "root";
-$password   = "";
-$dbname     = "php-crud";
+$servername = "localhost";  // لـ Back4app container
+$username = "root";
+$password = "";
+$dbname = "php-crud";
 
-// Create connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
-
-// Check connection
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
-// echo "Connected successfully";
 ?>
-
